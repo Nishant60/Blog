b6f4ef215f9ad904242160fa2a451fe955e3e1ed
Nishant60 <78270927+Nishant60@users.noreply.github.com>
2022-12-18 22:26:19 +0530

    final complete

---
+++ blog/frontend/src/components/Blogs.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Blog from "./Blog";
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState();
+  const sendRequest = async () => {
+    const res = await axios
+      .get("http://localhost:5000/api/blog")
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data.blogs));
+  }, []);
+  console.log(blogs);
+  return (
+    <div>
+      {blogs.length===0 && <div style={{
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        width:'100%',
+        height:'90vh'
+      }}>
+        <span style={{fontSize:'30px',fontWeight:'bold',textAlign:'center'}}>No Blogs Added Yet</span>
+        </div>}
+      {blogs &&
+        blogs.map((blog, index) => (
+          <Blog
+            id={blog._id}
+            isUser={localStorage.getItem("userId") === blog.user._id}
+            title={blog.title}
+            description={blog.description}
+            imageURL={blog.image}
+            userName={blog.user.name}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Blogs;
